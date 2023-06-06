4aa76a9db0d617c395df3a94f8affb55b1626742
atemmel <temmeladam@gmail.com>
2023-06-06 14:39:26 +0200

    HTML tests passing

--- src/html.zig
+++ src/html.zig
@@ -13,8 +13,7 @@ const Allocator = std.mem.Allocator;
 pub const Root = struct {
     pub const Node = struct {
         href: ?[]const u8,
-        inner_html: ?[]const u8,
-        name: []const u8,
+        inner_html: []const u8,
     };
 
     ally: Allocator,
@@ -26,10 +25,7 @@ pub const Root = struct {
             self.ally.free(title);
         }
         for (self.elements) |element| {
-            if (element.inner_html) |inner_html| {
-                self.ally.free(inner_html);
-            }
-            self.ally.free(element.name);
+            self.ally.free(element.inner_html);
         }
         self.ally.free(self.elements);
     }
@@ -75,46 +71,6 @@ pub fn parse(ally: Allocator, html: [:0]const u8) !Root {
     };
 }
 
-fn parseRecurse(ctx: *ParseCtx, doc: c.TidyDoc, node: c.TidyNode) !void {
-    var found_body = false;
-    defer {
-        if (found_body) {
-            ctx.enter_body = false;
-        }
-    }
-    var child = c.tidyGetChild(node);
-    while (child != null) {
-        defer child = c.tidyGetNext(child);
-        var maybe_name = c.tidyNodeGetName(child);
-        if (maybe_name) |name| {
-            const name_slice = span(name);
-            if (ctx.title == null and eql(u8, "title", name_slice)) {
-                try parseTitle(ctx, doc, child);
-                continue;
-            } else if (!ctx.enter_body and eql(u8, "body", name_slice)) {
-                ctx.enter_body = true;
-                try parseRecurse(ctx, doc, child);
-                continue;
-            }
-
-            if (ctx.enter_body) {
-                try ctx.elements.append(Root.Node{
-                    .name = try ctx.ally.dupe(u8, name_slice),
-                    .href = null,
-                    .inner_html = null,
-                });
-            }
-        } else {
-            var buffer = makeBuffer();
-            defer c.tidyBufFree(&buffer);
-            _ = c.tidyNodeGetText(doc, child, &buffer);
-            var elements = ctx.elements.items;
-            elements[elements.len - 1].inner_html = try dupeBuff(ctx.ally, buffer);
-        }
-        try parseRecurse(ctx, doc, child);
-    }
-}
-
 fn parseHtml(ctx: *ParseCtx) !void {
     var child = c.tidyGetChild(ctx.node);
     while (child != null) {
@@ -154,16 +110,26 @@ fn parseInnerHtml(ctx: *ParseCtx, node: c.TidyNode) ![]u8 {
 }
 
 fn parseBody(ctx: *ParseCtx, body: c.TidyNode) !void {
-    std.debug.print("found_body\n", .{});
     var child = c.tidyGetChild(body);
     while (child != null) {
         defer child = c.tidyGetNext(child);
+        try parseBodyElement(ctx, child);
+    }
+}
+
+fn parseBodyElement(ctx: *ParseCtx, parent: c.TidyNode) !void {
+    var child = c.tidyGetChild(parent);
+    while (child != null) {
+        defer child = c.tidyGetNext(child);
         const maybe_name = c.tidyNodeGetName(child);
-        if (maybe_name) |name| {
-            const name_slice = span(name);
-            if (eql(u8, "title", name_slice)) {
-                ctx.title = try parseInnerHtml(ctx, c.tidyGetChild(child));
-            }
+        if (maybe_name) |_| {
+            try parseBodyElement(ctx, child);
+        } else {
+            const inner_html = try parseInnerHtml(ctx, child);
+            try ctx.elements.append(.{
+                .href = null,
+                .inner_html = inner_html,
+            });
         }
     }
 }
@@ -197,13 +163,18 @@ test "html parse" {
     var tree = try parse(std.testing.allocator, html);
     defer tree.deinit();
 
+    const elements = tree.elements;
+
     std.debug.print("title: {?s}\n", .{tree.title});
-    for (tree.elements) |e| {
-        std.debug.print("name: {s}\n", .{e.name});
-        std.debug.print("inner_html: {?s}\n", .{e.inner_html});
+    for (elements) |e| {
+        std.debug.print("inner_html: {s}\n", .{e.inner_html});
     }
 
     try expectEqual(true, tree.title != null);
     try expectEqualStrings("Cool title", tree.title.?);
-    try expectEqual(@as(usize, 4), tree.elements.len);
+    try expectEqual(@as(usize, 4), elements.len);
+    try expectEqualStrings("The header", elements[0].inner_html);
+    try expectEqualStrings("The paragraph", elements[1].inner_html);
+    try expectEqualStrings("The other paragraph", elements[2].inner_html);
+    try expectEqualStrings("To google", elements[3].inner_html);
 }
