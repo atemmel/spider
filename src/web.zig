47a6924beac83c3dda89072a8a8b4fd767167b57
atemmel <temmeladam@gmail.com>
2023-06-08 23:44:33 +0200

    Can now display html

--- src/web.zig
+++ src/web.zig
@@ -1,25 +1,39 @@
 const std = @import("std");
+const html = @import("html.zig");
 const term = @import("term.zig");
 const logo = @import("logo.zig");
 const module = @import("module.zig");
 
 pub const Web = struct {
+    ally: std.mem.Allocator,
+    root: html.Root,
+
     pub fn init(ally: std.mem.Allocator) !Web {
-        _ = ally;
-        return Web{};
+        return Web{
+            .ally = ally,
+            .root = try html.parse(ally, @embedFile("resources/test.html")),
+        };
     }
 
     pub fn deinit(self: *Web) void {
-        _ = self;
+        self.root.deinit();
     }
 
     pub fn draw(self: *Web) void {
-        _ = self;
         term.erase();
         term.attrOn(term.color(2));
         logo.dumpCenter();
         term.attrOff(term.color(2));
         term.footer("web");
+        self.drawHtml();
+    }
+
+    fn drawHtml(self: *Web) void {
+        var y: u32 = 0;
+        for (self.root.elements) |node| {
+            term.mvSlice(y, 0, node.inner_html);
+            y += 1;
+        }
     }
 
     pub fn update(self: *Web, input: i32) module.Result {
